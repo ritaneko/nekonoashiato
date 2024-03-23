ad95a5bab119d78780f549ee8fdf5535df0208c3
EC2 Default User <ec2-user@ip-172-31-36-53.ap-northeast-1.compute.internal>
2024-03-23 13:45:27 +0000

    掲示板機能完成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,16 +1,20 @@
 class Public::PostsController < ApplicationController
   def create
-    post = current_user.posts.build(post_params)
-    if post.save
-      redirect_to board_path(params[:board_id]), success: t('defaults.message.created')
-    else
-      redirect_to board_path(params[:board_id]), danger: t('defaults.message.not_created')
-    end
+    board = Board.find(params[:board_id])
+    post = current_user.posts.new(post_params)
+    post.board_id = board.id
+    post.save
+    redirect_to board_path(board)
+  end
+  
+  def destroy
+    Post.find(params[:id]).destroy
+    redirect_to board_path(params[:board_id])
   end
   
   private
   
   def post_params
-    params.require(:post).permit(:body, :board_id).merge(board_id: params[:board_id])
+    params.require(:post).permit(:body, :board_id)
   end
 end
