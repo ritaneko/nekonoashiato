2961b213473839b3cd2f72b0d0067a3a7baad6f8
EC2 Default User <ec2-user@ip-172-31-36-53.ap-northeast-1.compute.internal>
2024-03-30 16:06:06 +0000

    seedファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -107,9 +107,10 @@ ActiveRecord::Schema.define(version: 2024_03_20_121447) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "tag_name"
+    t.string "tag_name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_name"], name: "index_tags_on_tag_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "seedの実行を開始"
+
+rita = User.find_or_create_by!(email: "rita@example.com") do |user|
+  user.name = "Rita"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/nekocyanPAKE4516-433_TP_V4.jpg"), filename:"nekocyanPAKE4516-433_TP_V4.jpg")
+end
+
+yuri = User.find_or_create_by!(email: "yuri@example.com") do |user|
+  user.name = "Yuri"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/nekocyanPAKE4478-424_TP_V4.jpg"), filename:"nekocyanPAKE4478-424_TP_V4.jpg")
+end
+
+miko = User.find_or_create_by!(email: "miko@example.com") do |user|
+  user.name = "Miko"
+  user.password = "password"
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/nekocatPAR585852415_TP_V4.jpg"), filename:"nekocatPAR585852415_TP_V4.jpg")
+end
+
+Photo.find_or_create_by!(title: "カメラ目線") do |photo|
+  photo.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/zeke-tucker-gSSK4u8yPpM-unsplash.jpg"), filename:"zeke-tucker-gSSK4u8yPpM-unsplash.jpg")
+  photo.body = "カメラ目線でゴロン"
+  photo.user = rita
+end
+
+Photo.find_or_create_by!(title: "窓辺でお昼寝") do |photo|
+  photo.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/roberto-huczek-oA6zqMdnhjE-unsplash.jpg"), filename:"roberto-huczek-oA6zqMdnhjE-unsplash.jpg")
+  photo.body = "気持ちよさそうに日向ぼっこ"
+  photo.user = yuri
+end
+
+Photo.find_or_create_by!(title: "お気に入り") do |photo|
+  photo.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/TOMS526533_TP_V4.jpg"), filename:"TOMS526533_TP_V4.jpg")
+  photo.body = "いいもの見つけた！"
+  photo.user = miko
+end
+
+Tag.create([
+  { tag_name: 'かわいい' },
+  { tag_name: 'おねむ' },
+  { tag_name: 'ユニーク' },
+  { tag_name: '不思議' }
+  ])
+  
+Board.find_or_create_by!(title: "お気に入りの猫") do |board|
+  board.body = "みんなの好きな猫種、毛色、模様色々教えて！"
+  board.user = rita
+end
+
+puts "seedの実行が完了しました"
